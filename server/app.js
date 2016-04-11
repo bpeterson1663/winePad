218532d5aed739697e12f456afd42c6c42fc3fce
bpeterson1663 <bpeterson1663@gmail.com>
2016-04-11 14:57:59 -0500

    Search and filter added

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(session({
     key: "user",
     resave: true,
     s: false,
-    cookie: {maxAge: 60000, secure: false}
+    cookie: {maxAge: 600000, secure: false}
 }));
 
 app.use(cookieParser());
