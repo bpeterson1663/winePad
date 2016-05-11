71dc0f1e0c65ba2928e5045cb177e7642b512a63
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-10 23:37:20 -0500

    minor changes

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//PASPORT
+//PASSPORT
 var passport = require("passport");
 var session = require("express-session");
 var localStrategy = require("passport-local");
