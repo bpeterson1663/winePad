a563e81d96369a573803a0b4898ffb9997110616
bpeterson1663 <bpeterson1663@gmail.com>
2016-09-08 18:47:48 -0500

    Building out report functionality

--- server/public/assets/scripts/controllers/ReportsController.js
+++ server/public/assets/scripts/controllers/ReportsController.js
@@ -2,5 +2,5 @@ myApp.controller('ReportsController', ['$scope', 'WineCellarService', function (
     var wineCellar = WineCellarService;
     wineCellar.checkUserLoggedIn();//checks if user is logged in and grabs their wine list
     $scope.wineList = wineCellar.wineList;//displays users wine list
-
+    console.log("Wine Data", wineCellar.wineList);
 }]);
